47b2a6ed376626a1222fb06aaa6c846929413929
jordanlianhs <97859704+jordanlianhs@users.noreply.github.com>
2023-06-02 19:32:15 +0800

    Lombok dependency

--- test/src/main/java/com/example/test/TestApplication.java
+++ test/src/main/java/com/example/test/TestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.test.other.User;
 import com.fasterxml.jackson.annotation.JsonGetter;
 
 @SpringBootApplication
@@ -20,6 +21,9 @@ public class TestApplication {
     }
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+        User  user = new User();
+        user.setName(name);
+
+      return String.format("Hello %s!", user.getName());
     }
 }
