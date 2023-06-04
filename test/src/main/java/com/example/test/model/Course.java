c0f0ae3dc25b2365746284c760bafbe9e1964f33
jordanlianhs <97859704+jordanlianhs@users.noreply.github.com>
2023-06-04 22:39:10 +0800

    Removing useless imports

--- test/src/main/java/com/example/test/model/Course.java
+++ test/src/main/java/com/example/test/model/Course.java
@@ -2,8 +2,6 @@ package com.example.test.model;
 
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 
--- test/src/main/java/com/example/test/model/Student.java
+++ test/src/main/java/com/example/test/model/Student.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToMany;
 
 import java.util.List;
