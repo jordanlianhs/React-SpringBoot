62943c650d8587a0436c495b1efaa5e1586bc4da
jordanlianhs <97859704+jordanlianhs@users.noreply.github.com>
2023-07-02 15:53:41 +0800

    same as prev commit

--- test/src/main/java/com/example/test/view/CourseView.java
+++
@@ -1,9 +0,0 @@
-package com.example.test.view;
-import java.util.List;
-
-import com.example.test.model.Course;
-public interface CourseView {
-    public Course saveCourse(Course course);
-
-    public List<Course> getAllCourses();
-}
--- test/src/main/java/com/example/test/view/StudentView.java
+++
@@ -1,9 +0,0 @@
-package com.example.test.view;
-import java.util.List;
-
-import com.example.test.model.Student;
-public interface StudentView {
-    public Student saveStudent(Student student);
-
-    public List<Student> getAllStudents();
-}
